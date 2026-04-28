aaf41762c5cd07958335b6c5418e00b533dd4dc5
Francesco Mercuri <fc.mercuri@gmail.com>
2026-04-28 19:36:49 +0100

    Replace AuthProvider with HelmetProvider in main.jsx

--- socialsage-frontend/src/main.jsx
+++ socialsage-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+)
